69824d8d682cfea4e17a51fab27fcbf04dd6ddfb
quanfeng <quanfeng@mjdefense.cn>
2021-10-12 12:20:44 +0800

    feat: hook封装Debounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject } from "../../utils";
+import { cleanObject,useDebounce,useMount } from "../../utils";
 import * as qs from "qs"
 
 const apiurl = process.env.REACT_APP_API_URL;
@@ -12,23 +12,25 @@ export const ProjectListScreen = () => {
     name: "",
     personId: "",
   });
+  const debounceParam = useDebounce(param,2000)
   const [list, setList] = useState([]);
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiurl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
+
   useEffect(() => {
     fetch(
-      `${apiurl}/projects?${qs.stringify(cleanObject(param) )}`
+      `${apiurl}/projects?${qs.stringify(cleanObject(debounceParam) )}`
     ).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
+  }, [debounceParam]);
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 export const cleanObject = (object) => {
   //Object.assign({}, object)
@@ -9,4 +10,17 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
 };
